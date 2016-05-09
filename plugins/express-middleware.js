90efc9f573549864e82168519c4a82fdca560983
Mora <qiuzhongleiabc@126.com>
2016-05-09 09:53:49 +0800

    fix: 服务端获取 EA 的 cookie 后马上就删除它

--- plugins/express-middleware.js
+++ plugins/express-middleware.js
@@ -16,6 +16,7 @@ module.exports = function (mockOptions) {
     }
 
     eaData = eaData || req.cookies['__ea' + key];
+    res.clearCookie('__ea' + key);
 
     try {
       http = base.parse(eaData);
